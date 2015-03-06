30d178ae660c0010523f485318e937559bb09304
ashrafeme <ashraf_eme@hotmail.com>
2015-03-07 00:39:00 +0300

    New Features

---
+++ app/src/main/java/hasaedu/gifted/base/BaseActionBarActivity.java
@@ -0,0 +1,11 @@
+package hasaedu.gifted.base;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Ashraf on Mar 06, 2015.
+ */
+public class BaseActionBarActivity extends ActionBarActivity {
+
+
+}
